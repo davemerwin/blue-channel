322814dbc9e42cf10f2cb11b68812721a5ff1468
Rob Hudson <rob@cogit8.org>
2008-05-06 15:00:57 -0700

    removing structure now that Page heirarchy defines structure

--- bluechannel/structure/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.views.generic.list_detail import object_detail
-from bluechannel.structure.models import *
-from bluechannel.page.models import *
-
-def section_base(request, slug):
-    slug_field=slug
-    section = Section.objects.filter(slug=slug)
-    return object_detail(request, slug=slug_field, queryset=section)
