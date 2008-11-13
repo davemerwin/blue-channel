42a688975e6e4010e1c3276d3cd061a86f918f2b
Dave Merwin <dmerwin@orcasinc.com>
2008-11-12 16:58:50 -0800

    creating named URLS

--- bluechannel/blog/urls.py
+++ bluechannel/blog/urls.py
@@ -8,9 +8,9 @@ info_dict = {
 }
 
 urlpatterns = patterns('django.views.generic.date_based',
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$', 'object_detail', info_dict),
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',               'archive_day',   info_dict),
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',                                'archive_month', info_dict),
-   (r'^(?P<year>\d{4})/$',                                                    'archive_year',  info_dict),
-   (r'^$',                                                                    'archive_index', info_dict),
+   url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$', 'object_detail', info_dict, name="detail"),
+   url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day',   info_dict, name="day-archive"),
+   url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', info_dict, name="month-archive"),
+   url(r'^(?P<year>\d{4})/$', 'archive_year',  info_dict, name="year-archive"),
+   url(r'^$', 'archive_index', info_dict, name="latest"),
 )
