40b2f7a44e5c1ee0c27ad5c77176bd177dd56498
David Merwin <dave@davemerwin.com>
2009-01-06 13:02:31 -0800

    correct urls settings

--- bluechannel/settings.py
+++ bluechannel/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'bluedummy.urls'
+ROOT_URLCONF = 'bluechannel.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
