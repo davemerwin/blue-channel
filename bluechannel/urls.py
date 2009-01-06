92433031be7253a9362e00bf22a5e77f6d8eb98e
David Merwin <dave@davemerwin.com>
2009-01-06 13:14:01 -0800

    updating urls for admin

--- bluechannel/urls.py
+++ bluechannel/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^bluedummy/', include('bluedummy.foo.urls')),
+    # (r'^bluechannel/', include('bluechannel.foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
