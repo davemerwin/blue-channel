1db1d4a73375c8fb432e59f68d1484870f2227f6
Rob Hudson <rhudson@orcasinc.com>
2009-07-28 15:11:16 -0700

    Implement page template overrides as provided in the Page model.  Also some view code cleanup.

--- bluechannel/page/views.py
+++ bluechannel/page/views.py
@@ -1,9 +1,9 @@
-from django.views.generic.list_detail import *
-from page.models import *
-from django.shortcuts import render_to_response
-from django.template import Context, Template, RequestContext
+from django.db import models
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
 from django.views.generic.list_detail import object_list, object_detail
-from django.http import Http404, HttpResponseRedirect
+
+Page = models.get_model('page', 'page')
 
 # see docs for how to grab the template view. Pretty straight forward
 
@@ -17,14 +17,20 @@ def published_page(request, slug):
     return object_detail(request, slug=slug_field, queryset=published_page)
 
 def detail(request, slug):
-    return object_detail(request, slug=slug, queryset=Page.published_objects.all())
+    page = get_object_or_404(Page, slug=slug)
+    template = page.template or 'page/page_detail.html'
+    return render_to_response(
+        template,
+        {'object': page},
+        context_instance=RequestContext(request)
+    )
     
 def home(request):
     try:
         home = Page.published_objects.get(is_home=True)
     except Page.DoesNotExist:
-	home = None
-        
+        home = None
+    
     return render_to_response(
         'page/homepage.html',
         {'home': home,},
