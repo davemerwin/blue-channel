df19be41fef3cea10371998eb6188e35de85b07b
Rob Hudson <rhudson@orcasinc.com>
2009-07-29 13:55:24 -0700

    Adding setup.py and license

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Blue Channel CMS',
+    version='0.1'
+    description='A content management system developed in Django, jQuery and 960.',
+    long_description='',
+    # Get more strings from http://www.python.org/pypi?:action=list_classifiers
+    author='Dave Merwin',
+    author_email='dave@davemerwin.com',
+    url='http://github.com/davemerwin/blue-channel/tree/master',
+    download_url='http://github.com/davemerwin/blue-channel/downloads',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
